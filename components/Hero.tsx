256f3acb9c88a949507538c13b5224c1789f89ac
nawabsahab16 <nawab162004@gmail.com>
2023-10-19 17:46:05 +0530

    feat: created camp section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -56,35 +56,40 @@ const Hero = () => {
                      icon="/play.svg"
                      variant="btn_white_text"
                    />
-
                 </div>
-         </div>
-             
+         </div>             
              <div className="relative flex flex-1 items-start">
-               <div className="relative z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8"> 
+               <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8"> 
+                  
+                  
                   <div className="flex flex-col">
                     <div className="flexBetween">
                       <p className="regular-16 text-grey-20"> Location   </p>
                        
-                       <Image 
+                        <Image 
                          src="/close.svg"
                          alt="close"
-                         width={24}
+                         width={24} 
                          height={24}
                        />
                      </div>
-                        
+                      
+                      <p className="bold-20 text-white"> Augus Calientes </p>
+                   </div>
+
+
                       <div className="flexBetween">
                         <div className="flex flex-col">
                           <p className="regular-16 block text-grey-20"> Distance 
                           </p>
                           <p className="bold-20 text-white"> 173.35 miles </p>
                         </div>
+                        <div className="flex flex-col">
+                          <p className="regular-16 block text-grey-20"> Elevation  
+                          </p>
+                          <p className="bold-20 text-white"> 2.040 KM </p>
+                        </div>
                       </div>
-
-
-                  
-                  </div>
                </div>
              </div>
 
